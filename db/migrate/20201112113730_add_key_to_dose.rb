4b4af551e40ce6ad6548e29f60ae543873e65b71
riccardo amici <amici95@gmail.com>
2020-11-13 11:49:56 +0100

    first ex

--- db/migrate/20201112113730_add_key_to_dose.rb
+++
@@ -1,6 +0,0 @@
-class AddKeyToDose < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :doses, :cocktail, index: true
-    add_reference :doses, :ingredient, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_113730) do
+ActiveRecord::Schema.define(version: 2020_11_12_140617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,10 @@ ActiveRecord::Schema.define(version: 2020_11_12_113730) do
 
   create_table "doses", force: :cascade do |t|
     t.text "description"
+    t.bigint "cocktail_id", null: false
+    t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "cocktail_id"
-    t.bigint "ingredient_id"
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
@@ -37,4 +37,6 @@ ActiveRecord::Schema.define(version: 2020_11_12_113730) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
