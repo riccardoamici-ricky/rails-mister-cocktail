4869fb527598e7e30846ae887329639b095ce8bd
riccardo amici <amici95@gmail.com>
2020-12-10 13:24:41 +0100

    footer changed

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
+  has_one_attached :photo
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:create, :index, :show, :new] do
+  resources :cocktails, only: [:create, :index, :show, :new, :destroy] do
     resources :doses, only: [:create, :show, :new]
   end
   resources :doses, only: :destroy
