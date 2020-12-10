d63981efd095903bc6a8df2fdae88d5d20404372
riccardo amici <amici95@gmail.com>
2020-12-10 13:48:59 +0100

    bug fixes on heroku

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module RailsMisterCocktail
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end
